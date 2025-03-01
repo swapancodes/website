850ec5c9f5b7d2c64b1f1c9f233f67d8c3a1a883
Swapan <sksarkar2outlook@gmail.com>
2025-03-01 20:48:26 +0530

    first

---
+++ script.js
@@ -0,0 +1,6 @@
+function togglemenu(){
+    cont menu=document.querySelector(".menu-link");
+    cont icon=document.querySelector(".humbergur-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
