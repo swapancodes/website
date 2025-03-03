02a3f4e9b9e48372dbfc428718461bfc1dca6a95
Swapan <sksarkar2outlook@gmail.com>
2025-03-03 20:43:34 +0530

    001

--- script.js
+++
@@ -1,6 +0,0 @@
-function togglemenu(){
-    cont menu=document.querySelector(".menu-link");
-    cont icon=document.querySelector(".humbergur-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-}
---
+++ templates/script.js
@@ -0,0 +1,6 @@
+function toggleMenu(){
+    const menu=document.querySelector(".menu-link");
+    const icon=document.querySelector(".humbergur-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
